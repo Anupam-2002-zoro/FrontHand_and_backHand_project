588e29d7d63cb9a6ab3361bafa3ff2622058e4db
rock <yourRocky@gmail.com>
2026-02-05 19:49:07 +0530

    first 5/2

--- touristplaces/src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind CSS v3 Working 🚀 
-      </h1>
-    </div>
-  )
-}
