47accaa3aa0bb75cebce303c19406b7e826eb6f1
rock <yourRocky@gmail.com>
2026-02-16 13:47:46 +0530

    posaka add

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import MainImg from "../assets/home.jpeg"
 
+
 const Home = () => {
+
   return (
-    <div className=' w-full h-[80vh] mt-4'>
-      <img src={MainImg} alt='mainimg'/>
-      
-    </div>
+    <>
+    
+    </>
   )
 }
 
